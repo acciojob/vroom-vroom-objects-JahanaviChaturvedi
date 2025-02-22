beada9347239d049a412654865de0398504b75ac
JahanaviChaturvedi <118982130+JahanaviChaturvedi@users.noreply.github.com>
2025-02-22 10:52:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ Car.prototype.getMakeModel = function(){
 
 function SportsCar(make, model, topSpeed) {
 	Car.call(this,make,model);
-	this.toSpeed = topSpeed;
+	this.topSpeed = topSpeed;
 }
 SportsCar.prototype = Object.create(Car.prototype);
 SportsCar.prototype.constructor = SportsCar;
